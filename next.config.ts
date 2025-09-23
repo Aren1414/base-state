458bb7901962c64b7ad30f535693015c7cd0cb1a
Aren <Aren.silver12@gmail.com>
2025-09-23 20:52:51 +0330

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,9 +1,24 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  webpack: (config) => {
+  webpack: (config, { isServer }) => {
+    
+    config.externals = config.externals || [];
     config.externals.push("pino-pretty", "lokijs", "encoding");
+
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...(config.resolve.fallback || {}),
+        "async-storage": false,
+        "@react-native-async-storage/async-storage": false,
+      };
+    }
+
     return config;
+  },
+  eslint: {
+    
+    ignoreDuringBuilds: true,
   },
 };
 
