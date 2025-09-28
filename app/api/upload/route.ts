7704d75756b06ecb5b1bc44657dfdecca09eb066
Aren <Aren.silver12@gmail.com>
2025-09-28 18:43:11 +0330

    Update route.ts

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,6 +1,6 @@
 import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3'
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
-import { storjBucket, s3 } from '../../../../../src/lib/storjClient'
+import { storjBucket, s3 } from '@lib/storjClient'
 
 export const runtime = 'nodejs'
 
