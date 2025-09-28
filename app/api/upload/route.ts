02ac12890b7d04eea9c9950677c896fffcf1f2fa
Aren <Aren.silver12@gmail.com>
2025-09-28 19:15:50 +0330

    Update route.ts

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -1,4 +1,4 @@
-import { PutObjectCommand, S3Client } from '@aws-sdk/client-s3'
+import { PutObjectCommand } from '@aws-sdk/client-s3'
 import { getSignedUrl } from '@aws-sdk/s3-request-presigner'
 import { storjBucket, s3 } from '@lib/storjClient'
 
@@ -14,10 +14,14 @@ export async function GET() {
       ContentType: 'image/png',
     })
 
-    const url = await getSignedUrl(s3, command, { expiresIn: 60 * 60 }) // 1 ساعت معتبر
+    
+    const uploadUrl = await getSignedUrl(s3, command, { expiresIn: 600 })
+
+    
+    const downloadUrl = `https://link.storjshare.io/s/${storjBucket}/${fileName}`
 
     return new Response(
-      JSON.stringify({ url, fileName }),
+      JSON.stringify({ uploadUrl, downloadUrl }),
       { status: 200, headers: { 'Content-Type': 'application/json' } }
     )
   } catch (err: unknown) {
