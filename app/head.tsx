f78481da68a695a4b859779b5c4af2963d1a7e4c
Aren <Aren.silver12@gmail.com>
2025-10-03 16:36:49 +0330

    Update head.tsx

--- app/head.tsx
+++ app/head.tsx
@@ -2,18 +2,6 @@ export default function Head() {
   return (
     <>
       <title>BaseState Mini App</title>
-      <meta name="fc:frame" content='{
-        "version": "next",
-        "imageUrl": "https://base-state.vercel.app/embed.png",
-        "button": {
-          "title": "Check Wallet Stats",
-          "action": {
-            "type": "launch_frame",
-            "name": "base-state",
-            "url": "https://base-state.vercel.app"
-          }
-        }
-      }' />
     </>
   )
 }
