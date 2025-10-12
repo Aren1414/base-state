c2a90684834f8b146cfb03650d1fe37db5d1ceec
Aren <Aren.silver12@gmail.com>
2025-10-12 16:21:05 +0330

    Update route.ts

--- app/api/metadata/[id]/route.ts
+++ app/api/metadata/[id]/route.ts
@@ -1,19 +1,20 @@
 import { NextRequest } from "next/server"
 
 const PUBLIC_GATEWAY = "https://link.storjshare.io/raw"
-const BUCKET = process.env.STORJ_BUCKET as string
+const BUCKET = "wallet-cards" 
+const STORJSHAREID = "jwehpt5oybcnyzdpzgkvbodeireq" 
 
 export async function GET(request: NextRequest) {
   try {
     const urlSegments = request.nextUrl.pathname.split("/")
     const fileName = urlSegments[urlSegments.length - 1]
 
-    const imageUrl = `${PUBLIC_GATEWAY}/${BUCKET}/${fileName}`
+    const imageUrl = `${PUBLIC_GATEWAY}/${STORJSHAREID}/${BUCKET}/${fileName}`
 
     const metadata = {
       name: `BaseStateCard ${fileName.replace(".png", "")}`,
       description: "BaseState wallet/contract stats as NFT",
-      image: imageUrl, 
+      image: imageUrl,
     }
 
     return new Response(JSON.stringify(metadata), {
