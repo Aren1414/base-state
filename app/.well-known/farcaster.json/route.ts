5fd6b4e9d71f110da3579d4a1b7d484cc82df58c
Aren <Aren.silver12@gmail.com>
2025-10-15 15:40:37 +0330

    Update route.ts

--- app/.well-known/farcaster.json/route.ts
+++ app/.well-known/farcaster.json/route.ts
@@ -2,14 +2,16 @@ import { withValidManifest } from "@coinbase/onchainkit/minikit";
 import { minikitConfig } from "../../../minikit.config";
 
 export async function GET() {
-  
   const manifest = withValidManifest(minikitConfig);
 
   return Response.json({
     ...manifest,
     miniapp: {
       ...manifest.miniapp,
-      noindex: false   
+      noindex: false, 
+      canonicalDomain: minikitConfig.miniapp.canonicalDomain,
+      requiredChains: minikitConfig.miniapp.requiredChains,
+      requiredCapabilities: minikitConfig.miniapp.requiredCapabilities
     },
     baseBuilder: minikitConfig.baseBuilder
   });
