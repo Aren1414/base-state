298dda7dab19cff648f41ca2168016be7ba9125b
Aren <Aren.silver12@gmail.com>
2026-06-28 00:51:32 +0330

    Refactor facilitator client initialization

--- api/index.js
+++ api/index.js
@@ -2,20 +2,18 @@ import express from "express";
 import { paymentMiddleware, x402ResourceServer } from "@x402/express";
 import { ExactEvmScheme } from "@x402/evm/exact/server";
 import { HTTPFacilitatorClient } from "@x402/core/server";
+import { facilitator } from "@coinbase/x402";
 import { BUILDER_CODE, declareBuilderCodeExtension } from "@x402/extensions/builder-code";
 
 const app = express();
 app.use(express.json());
 
-
 const payTo = "0xb46043d161bde18ef6974217a686f381b1e91138";
 
+// MAINNET facilitator (CDP)
+const facilitatorClient = new HTTPFacilitatorClient(facilitator);
 
-const facilitatorClient = new HTTPFacilitatorClient({
-  url: "https://x402.org/facilitator",
-});
-
-// Register Base mainnet (8453)
+// Base MAINNET (8453)
 const server = new x402ResourceServer(facilitatorClient)
   .register("eip155:8453", new ExactEvmScheme());
 
